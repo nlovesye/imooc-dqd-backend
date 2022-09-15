91a9da446297a0d1b9c4e9635f134882d8c09ac1
nlovesye <894765963@qq.com>
2022-09-15 17:34:06 +0800

    feat: test webhook

--- src/index.js
+++ src/index.js
@@ -36,7 +36,6 @@ if (!isDevMode) {
 app.use(middleware);
 app.use(router());
 
-// listening
 app.listen(API_LISTENING_PORT, () => {
   console.log(`>>> RESTFUL API listening on port: ${API_LISTENING_PORT}`);
 });
